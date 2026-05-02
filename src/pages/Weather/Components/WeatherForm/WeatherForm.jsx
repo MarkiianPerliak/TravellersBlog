04c7d9a91143f64cda35ee268c0b370764b41b1b
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2026-05-02 18:08:59 +0300

    cmit

--- src/pages/Weather/Components/WeatherForm/WeatherForm.jsx
+++ src/pages/Weather/Components/WeatherForm/WeatherForm.jsx
@@ -12,14 +12,21 @@ const handleClick = (e) => {
     const city = e.target.city.value
     sendData(city);
   };
+
+
   return (
-    <section className={style.weather__formSection}>
+    <section className={style.weather__formS}>
         <Container>
-            <h1>Погода</h1>
-            <p>Створіть свій особистий список улюблених міст і завжди будьте в курсі погоди.</p>
-            <form onSubmit={handleClick} action="">
-                <input name="city" type="text" />
-                <button type="submit"><Search color="black" size={"25px"}/></button>
+            <h1 className={style.weather__headline}>Погода</h1>
+            <ul className={style.weather__list}>
+                <li className={style.weather__item}><p className={style.weather__headline}>Створіть свій особистий список улюблених міст і завжди будьте в курсі погоди.</p></li>
+                <li className={style.weather__item}><div className={style.weather__line}></div></li>
+                <li className={style.weather__item}><p className={style.weather__headline}>Створіть свій особистий список улюблених міст і завжди будьте в курсі погоди.</p></li>
+            </ul>
+            
+            <form className={style.weather__form} onSubmit={handleClick} action="">
+                <input className={style.weather__location} placeholder="Пошук лакації" name="city" type="text" />
+                <button className={style.weather__submit} type="submit"><Search color="black" size={"25px"}/></button>
             </form>
         </Container>
     </section>
