a69abd1928be3c8885ccf23faa704cd3b9f394f3
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2026-05-16 11:32:58 +0300

    styles

--- src/API/News/getAPI.jsx
+++ src/API/News/getAPI.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 export const getAPI = async (city = "Ukraine", page = "1") => {
     try {
         const apiKey = import.meta.env.VITE_newskey
-        const data = await axios.get(`https://newsapi.org/v2/everything?q=${city}&from=2026-04-13&sortBy=publishedAt&apiKey=${apiKey}&page=${page}&pageSize=4`)
+        const data = await axios.get(`https://newsapi.org/v2/everything?q=${city}&from=2026-05-13&sortBy=publishedAt&apiKey=${apiKey}&page=${page}&pageSize=4`)
         return data.data.articles
 
     } catch(error) {
