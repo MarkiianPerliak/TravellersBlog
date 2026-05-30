35fc6ee56b6e39454c98af897b4cf5289d2c967c
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2026-05-30 22:32:14 +0300

    fornow

--- src/pages/Weather/Components/DailyForecast/DailyForecast.jsx
+++ src/pages/Weather/Components/DailyForecast/DailyForecast.jsx
@@ -5,26 +5,32 @@ import style from "./DailyForecast.module.css";
 import { Container } from "../../../../components/Container/Container.jsx";
 import { Line } from 'react-chartjs-2';
 
+
 export const DailyForecast = ({selectedW}) => {
-    const [daily, setDaily] = useState([])
+    const [daily, setDaily] = useState(null)
+    const [rdyarray, setRdyarray] = useState([])
     useEffect(() => {
         getAPIN(selectedW.name).then(data => setDaily(data))
     }, [])
 
-    useEffect(() => {
-        console.log(daily.list)
-    }, [daily])
+useEffect(() => {
+    if (!daily?.list) return;
+
+    const temps = daily.list.slice(0, 8).map(item => Math.round(item.main.temp * 10) / 10);
+    setRdyarray(temps);
+}, [daily]);
+
 
   return (
     <section>
         <Container>
-            {/* <Line
-                datasetIdKey=""
+            <Line
+                datasetIdKey="id"
                 data={{
                     labels: ['3:00', '6:00', '9:00', '12:00', '15:00', '18:00', '21:00', '0:00'],
-                    datasets: [daily.list]
+                    datasets: rdyarray
                 }}
-                /> */}
+                />
         </Container>
     </section>
   )
